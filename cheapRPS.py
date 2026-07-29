afd9cdb161dca96c9d3ffc97b3870e7c8bada732
WhylerLab <development@noahwhyler.com>
2026-07-29 20:44:35 +0200

    Final uplad with some comments

--- cheapRPS.py
+++ cheapRPS.py
@@ -1,14 +1,37 @@
 import random
 
-# Randon choice
-symbols = ("rock", "paper", "scissors")
-
-cpu = random.choice(symbols)
-player = random.choice(symbols)
-
-
-# Conditions
+wins_against = {
+    "rock": "scissors",
+    "scissors": "paper",
+    "paper": "rock"
+}
 
 
+# Spieler gibt seine Wahl ein
+playerChoice = str(input("rock, paper or scisoors? Choose: "))
 
-# Output
+
+# CPU wählt zufällig
+cpuOptions = ("rock", "paper", "scissors")
+cpuChoice = random.choice(cpuOptions)
+
+
+# Beide Wahlen ausgeben
+print(playerChoice)
+print(cpuChoice)
+
+
+# Prüfen ob Spieler == CPU → Unentschieden
+if playerChoice == cpuChoice:
+    print("Unentschieden!")
+    exit()
+
+
+# Prüfen ob wins_against[player] == CPU Spieler gewinnt
+# Ansonsten CPU gewinnt
+if wins_against[playerChoice] == cpuChoice:
+    print("Du hast gewonnen!")
+else:
+    print("Du hast verloren!")
+
+
